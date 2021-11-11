d517790038ac4ca0f9c5414a55146dd04a1b308e
Gina Sprint <sprint@gonzaga.edu>
2021-11-11 11:56:21 -0800

    start of notes

--- app/src/main/java/com/sprint/gina/sqlitefuns1/MainActivity.java
+++ app/src/main/java/com/sprint/gina/sqlitefuns1/MainActivity.java
@@ -11,6 +11,21 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+// a database is like an excel workbook
+// a database table is like a workbook sheet
+// the table has rows and columns (just like an excel spreadsheet)
+// rows are uniquely identified by ids
+// columns are uniquely identfied by fields (name + type)
+
+// we want a contactsDatabase with a single table, contactsTable
+// contactsTable has 4 fields (_id, name, phoneNumber, imageResourceId)
+// we will insert a new row into the contactsTable for each contact
+
+// 2 classes to know
+// 1. SQLiteOpenHelper: we will subclass this class and add some SQL methods
+// CRUD: create, read, update, delete
+// 2. SQLiteDatabase: a reference to the database (read or write)
+
 public class MainActivity extends AppCompatActivity {
     static final String TAG = "MainActivityTag";
 
